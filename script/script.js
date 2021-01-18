e55be7fed5a433cbece1b6184801d4960e51c92d
DmitryOlegovi4 <dmitryolegovi4@gmail.com>
2021-01-18 12:49:37 +0300

    init

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const btnMenu = document.querySelector('.btnMenu');
+const navMenu = document.querySelector('.nav-menu');
+const navContacts = document.querySelector('.nav-contacts');
+
+btnMenu.addEventListener('click', function () {
+    if (window.innerWidth<=800){
+        const navMenuDisplay = window.getComputedStyle(navMenu).getPropertyValue('display');
+        if(navMenuDisplay === 'none'){
+            navMenu.style.display = 'flex';
+            btnMenu.innerHTML = '&#10006;';
+        } else {
+            navMenu.style.display = 'none';
+            btnMenu.innerHTML = '&#9776;';
+        }
+    }
+    if (window.innerWidth<=500) {
+        const navContactsDisplay = window.getComputedStyle(navContacts).getPropertyValue('display');
+        if(navContactsDisplay === 'none'){
+            navContacts.style.display = 'flex';
+        } else {
+            navContacts.style.display = 'none';
+        }
+    }
+})
