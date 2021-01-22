87eb744322de2423fe297e6932a3087d4ba53920
DmitryOlegovi4 <dmitryolegovi4@gmail.com>
2021-01-22 13:35:33 +0300

    change1

--- script/script.js
+++ script/script.js
@@ -3,22 +3,22 @@ const navMenu = document.querySelector('.nav-menu');
 const navContacts = document.querySelector('.nav-contacts');
 
 btnMenu.addEventListener('click', function () {
-    if (window.innerWidth<=800){
-        const navMenuDisplay = window.getComputedStyle(navMenu).getPropertyValue('display');
-        if(navMenuDisplay === 'none'){
-            navMenu.style.display = 'flex';
-            btnMenu.innerHTML = '&#10006;';
-        } else {
-            navMenu.style.display = 'none';
-            btnMenu.innerHTML = '&#9776;';
-        }
+    const navMenuDisplay = window.getComputedStyle(navMenu).getPropertyValue('display');
+    if(navMenuDisplay === 'none'){
+        navMenu.style.display = 'flex';
+        btnMenu.innerHTML = '&#10006;';
+    } else {
+        navMenu.style.display = 'none';
+        btnMenu.innerHTML = '&#9776;';
     }
+
     if (window.innerWidth<=500) {
-        const navContactsDisplay = window.getComputedStyle(navContacts).getPropertyValue('display');
-        if(navContactsDisplay === 'none'){
-            navContacts.style.display = 'flex';
-        } else {
-            navContacts.style.display = 'none';
-        }
+            const navContactsDisplay = window.getComputedStyle(navContacts).getPropertyValue('display');
+            if(navContactsDisplay === 'none'){
+                navContacts.style.display = 'flex';
+            } else {
+                navContacts.style.display = 'none';
+            }
     }
 })
+
